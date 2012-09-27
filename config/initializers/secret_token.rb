aa828247a0cc42c720435fca13fc3f14106bd46e
Elisa Verratti <elisa@lookoutsocial.com>
2012-09-27 09:22:54 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c14a9ef5f13623ee3fc38f3336a9933daa1687f5a7e216dbc7632e5a693caad61dbdbb973f878a223c16d405d520a434fb65572f40e3c40eb82b9b0de89dffb8'
